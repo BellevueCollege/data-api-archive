24a70e6a771e04e1d5339d5d7b2cca3f263f4c93
Nicole Swan <nicole.swan@bellevuecollege.edu>
2017-07-24 16:12:10 -0700

    Manually merge some changes to avoid merge cnflct

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,26 @@ $app->get('/', function () use ($app) {
 $app->group(['prefix' => 'v1'], function($app)
 {
     $app->post('auth/login', 'AuthController@loginPost');
+
+    $app->get('subject','SubjectController@index');
+  
+    $app->get('subject/{slug}','SubjectController@getSubject');
+    
+    /*$app->get('courses', function () use ($app) { 
+        return 'Hi. Do not try to see that many courses.';
+    });*/
+  
+    $app->get('courses/multiple', 'CourseController@getMultipleCourses');
+    //$app->get('courses/{courseid}','CourseController@getCourse');
+    
+    $app->get('quarters/current', 'YearQuarterController@getCurrentYearQuarter');
+    
+    //API endpoints specific to ModoLabs requirements
+    $app->get('catalog/terms', 'YearQuarterController@getViewableYearQuarters');
+    $app->get('catalog/terms/{yqrid}', 'YearQuarterController@getYearQuarter');
+    $app->get('catalog/catalogAreas/{yqrid}', 'SubjectController@getSubjectsByYearQuarter');
+    $app->get('catalog/{yqrid}/{subjectid}', 'CourseYearQuarterController@getCourseYearQuartersBySubject');
+    $app->get('catalog/{yqrid}/{subjectid}/{coursenum}', 'CourseYearQuarterController@getCourseYearQuarter');
 });
 
 /*$app->group(['prefix' => 'v1'], function ($app)
